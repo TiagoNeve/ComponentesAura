5ba52b56f5eada6386cd86a1c811b3e06c8289f3
Tiago Neves <rykarumstuler@gmail.com>
2022-01-25 00:00:41 -0300

    Fim do Inserir dados usando formulários

--- NocoesBasicaComponentesAuraController.js
+++ NocoesBasicaComponentesAuraController.js
@@ -27,5 +27,21 @@
 
 	handleClick3: function(component, event, helper) {
 		component.set("v.message", event.getSource().get("v.label"));
+	},
+	
+	clickCreate: function(component, event, helper) {
+		let validExpense = component.find('expenseform').reduce( (validSoFar, inputCmp) => {
+			// Displays error messges for invalid fields
+			inputCmp.showHelpMessageIdInvalid();
+			return validSoFar && inputCmp.get('v.validity').valid;
+		}, true );
+		// If we pass error checking, do some real work
+		if(validExpense){
+			// Create the new expense
+			let newExpense = component.get("v.newExpense");
+			console.log("Create expense: " + JSON.stringify(newExpense));
+			helper.createExpense(component, newExpense);
+		}
 	}
 })
+
--- aplication.app
+++ aplication.app
@@ -1,4 +1,4 @@
-<aura:application>
+<aura:application extends="force:slds">
 	<!-- Ele busca dentro do namespace padrão da org o component helloWorld, caso encontre então chama ele aq dentro do aplication.
 		Componentes também podem ter outros componentes.-->
 	<c:helloWorld/>
