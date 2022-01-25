96f48de412d867d627e2c458d3aea17cefc9bb8a
Tiago Neves <rykarumstuler@gmail.com>
2022-01-24 22:23:59 -0300

    Fim do Tratar de ações com controladores

--- NocoesBasicaComponentesAuraController.js
+++ NocoesBasicaComponentesAuraController.js
@@ -10,5 +10,22 @@
 		updateEvent.setParams({ "expense": expense });
 		// Executando o evento
 		updateEvent.fire();
+	},
+
+	handleClick: function(component, event, helper) {
+		let btnClicked = event.getSourc();
+		let btnMessage = btnClicked.get("v.label");
+		component.set("v.message", btnMessage);
+	},
+
+	handleClick2: function(component, event, helper) {
+		let newMessage = event.getSource().get("v.label");
+		console.log("handleClick2: Message: "+ newMessage);
+		JSON.stringify(newMessage);
+		component.set("v.message", newMessage);
+	},
+
+	handleClick3: function(component, event, helper) {
+		component.set("v.message", event.getSource().get("v.label"));
 	}
 })
