1b627399b5e66bd75da70cffd1b44ea665842312
Tiago Neves <rykarumstuler@gmail.com>
2022-01-30 14:22:15 -0300

    Fim do Inserir dados usando formulários

--- NocoesBasicaComponentesAuraController.js
+++ NocoesBasicaComponentesAuraController.js
@@ -42,6 +42,15 @@
 			console.log("Create expense: " + JSON.stringify(newExpense));
 			helper.createExpense(component, newExpense);
 		}
+	},
+
+	doInit : function(component, event, helper) {
+		let mydate = component.get("v.expense.Date__c");
+		if (mydate) {
+			component.set("v.formatdate", new Date(mydate));
+		}
 	}
+
+	
 })
 
